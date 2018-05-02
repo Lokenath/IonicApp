4c49149fbaea1a4becddb4a040b0dfed113036c4
Lokenath Singh <losingh@microsoft.com>
2018-05-02 12:24:22 +0530

    Accordian control implemented

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -10,6 +10,23 @@ export class HelloIonicPage {
   	this.showMapActionContent = false;
   }
 
+  ionViewDidEnter(){
+  	let acc = document.getElementsByClassName("accordion");
+	let i;
+
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+	    this.classList.toggle("active");
+	    let panel = this.nextElementSibling;
+	    if (panel.style.maxHeight){
+	      panel.style.maxHeight = null;
+	    } else {
+	      panel.style.maxHeight = panel.scrollHeight + "px";
+	    } 
+	  });
+	}
+  };
+
   mapActionContentToggle(){
   	this.showMapActionContent =  !this.showMapActionContent;
   }
